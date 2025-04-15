33e2ba98836ef16688159d234cf9877e68447cb2
Leonardo Andrade <leonardofandrade@gmail.com>
2025-04-15 13:41:38 -0300

    Add service_manager/urls.py 1744735296418

--- service_manager/urls.py
+++ service_manager/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
+
+def redirect_to_orders(request):
+    return redirect('orders:order_list')
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', redirect_to_orders, name='home'),
+    path('admin/', admin.site.urls),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
